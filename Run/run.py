246dab0952a49e96338f60b38aa74b5793104c63
Shenpx <shenpx91@gmail.com>
2019-07-16 20:18:11 +0800

    There is a bug in previous version: Humidity>100%. So the threshold is added to avoid the abnomal values

--- Run/run.py
+++ Run/run.py
@@ -10,9 +10,11 @@ def run_collection():
     while True:
         #h, t= dht.read_retry( dht.DHT22, 4 )
         h, t= dht.read_retry( dht.AM2302, 4 )
+        f_h = float(h)
         if t is not None and h is not None:
-            logging.info('Temp={0:0.1f}C and Humidity={1:0.1f}%'.format(t, h))
-            print( round(t, 2), round(h, 2))
-            break
+            if f_h<100:
+                logging.info('Temp={0:0.1f}C and Humidity={1:0.1f}%'.format(t, h))
+                print( round(t, 2), round(h, 2))
+                break
 
 run_collection()
