7b2e5bf7bca3ce919f476caa56ac623808bc5709
Shenpx <shenpx91@gmail.com>
2019-07-16 20:09:30 +0800

    Add another method to read; And change sensor name DHT22 -> AM2302

--- Run/run.py
+++ Run/run.py
@@ -8,7 +8,8 @@ logging.basicConfig( filename='/home/pi/Work/DHT22/Run/Log/Temp_Humi.log', filem
 
 def run_collection():
     while True:
-        h, t= dht.read_retry( dht.DHT22, 4 )
+        #h, t= dht.read_retry( dht.DHT22, 4 )
+        h, t= dht.read_retry( dht.AM2302, 4 )
         if t is not None and h is not None:
             logging.info('Temp={0:0.1f}C and Humidity={1:0.1f}%'.format(t, h))
             print( round(t, 2), round(h, 2))
